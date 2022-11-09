278485ccfad39e0a09cf8d476de6be03b9550030
LeGion <mehrdad.dadashi@gmail.com>
2022-11-09 18:30:33 +0300

    add floating button into homeScreen.dart

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -21,86 +21,107 @@ class HomeScreenState extends State<HomeScreen> {
                 left: 20,
                 right: 20,
               ),
-              child: Column(
+              child: Stack(
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Image(
-                        width: 30,
-                        height: 30,
-                        image: AssetImage("assets/images/logo.png"),
-                      ),
-                      Icon(Icons.menu),
-                    ],
-                  ), //appBar
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    margin: const EdgeInsets.symmetric(vertical: 10),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 6),
-                          child: Text(
-                            'کارها رو مدیریت کن',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey[800],
-                                fontSize: 16),
+                  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Image(
+                            width: 30,
+                            height: 30,
+                            image: AssetImage("assets/images/logo.png"),
                           ),
+                          Icon(Icons.menu),
+                        ],
+                      ), //appBar
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 6),
+                              child: Text(
+                                'کارها رو مدیریت کن',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey[800],
+                                    fontSize: 16),
+                              ),
+                            ),
+                            Text(
+                              'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.',
+                              style: TextStyle(color: Colors.grey[600]),
+                            )
+                          ],
                         ),
-                        Text(
-                          'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.',
-                          style: TextStyle(color: Colors.grey[600]),
-                        )
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                      child: ListView.builder(
-                          itemCount: 10,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Container(
-                              margin: const EdgeInsets.only(bottom: 10),
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                      child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
+                      ),
+                      Expanded(
+                          child: ListView.builder(
+                              itemCount: 10,
+                              itemBuilder: (BuildContext context, int index) {
+                                return Container(
+                                  margin: const EdgeInsets.only(bottom: 10),
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(10)),
+                                  child: Row(
                                     children: [
-                                      Text(
-                                        'کارها شماره 1',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.grey[800],
-                                        ),
-                                      ),
-                                      Text(
-                                        'توضیحات کار شماره 1توضیحات کار شماره 1',
-                                        style: TextStyle(
-                                          color: Colors.grey[600],
-                                        ),
+                                      Expanded(
+                                          child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            'کارها شماره 1',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.grey[800],
+                                            ),
+                                          ),
+                                          Text(
+                                            'توضیحات کار شماره 1توضیحات کار شماره 1',
+                                            style: TextStyle(
+                                              color: Colors.grey[600],
+                                            ),
+                                          )
+                                        ],
+                                      )),
+                                      const Icon(
+                                        Icons.delete,
+                                        color: Colors.red,
+                                        size: 20,
                                       )
                                     ],
-                                  )),
-                                  const Icon(
-                                    Icons.delete,
-                                    color: Colors.red,
-                                    size: 20,
-                                  )
-                                ],
-                              ),
-                            );
-                          }))
+                                  ),
+                                );
+                              })),
+                    ],
+                  ),
+                  Positioned(
+                      bottom: 10,
+                      left: 0,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.red,
+                        ),
+                        child: const Icon(
+                            Icons.add,
+                        color: Colors.white,
+                        ),
+                      )
+
+                  )
                 ],
               ),
             ),
