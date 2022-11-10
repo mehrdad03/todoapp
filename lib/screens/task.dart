111a3b42e2ef8fd31f05a961486d75532e086834
LeGion <mehrdad.dadashi@gmail.com>
2022-11-10 18:08:40 +0300

    add input filed to get task title in task.dart

--- lib/screens/task.dart
+++ lib/screens/task.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
-class TaskScreen extends StatefulWidget{
+class TaskScreen extends StatefulWidget {
   const TaskScreen({super.key});
 
   @override
-  State<StatefulWidget> createState()=>TaskScreenState();
+  State<StatefulWidget> createState() => TaskScreenState();
 }
 
-class TaskScreenState extends State<TaskScreen>{
+class TaskScreenState extends State<TaskScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Container(
-        child: Center(
-          child: GestureDetector(
-            onTap: (){
-              Navigator.pop(context);
-            },
-            child: Text("سلام"),
+    return Directionality(
+        textDirection: ui.TextDirection.rtl,
+        child: Scaffold(
+          body: SafeArea(
+            child: Container(
+              padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(
+                          Icons.arrow_back_rounded,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                      const Expanded(
+                          child: Padding(
+                        padding: EdgeInsets.only(right: 20),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              hintText: 'عنون کار شما',
+                              border: InputBorder.none),
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ))
+                    ],
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
-
 }
